c40068f0a9f6c8e678c61713feeb76220fc2af35
Tanner Evins <tanner.evins@gmail.com>
2014-09-06 15:13:41 -0500

    Add broadcast mechanism to State base class
    Decouple State from observers by providing an interface (IStateListener)
    through which listeners may receive state change requests. StateAction
    enumerates stack operations.

--- Source/Starboard_2013/Starboard_2013/State.cpp
+++ Source/Starboard_2013/Starboard_2013/State.cpp
@@ -5,13 +5,69 @@ namespace temm
 {
 
 	State::State()
-		: m_pStateStack(nullptr)
+		: m_Listeners()
 	{
 	}
 
-	State::State(StateStack& ss)
-		: m_pStateStack(&ss)
+	void State::AttachListener(std::shared_ptr<IStateListener> pListener)
 	{
+		for (auto it = m_Listeners.begin(); it != m_Listeners.end(); ++it)
+		{
+			if (auto stp = it->lock())
+			{
+				if (pListener.get() == stp.get())
+				{
+					return;
+				}
+			}
+		}
+		// If it's not already in m_Listeners
+		std::weak_ptr<IStateListener> wpListener(pListener);
+		m_Listeners.push_back(wpListener);
+	}
+
+	void State::DetachListener(std::shared_ptr<IStateListener> pListener)
+	{
+		for (auto it = m_Listeners.begin(); it != m_Listeners.end(); ++it)
+		{
+			if (auto stp = it->lock())
+			{
+				if (pListener.get() == stp.get())
+				{
+					m_Listeners.erase(it);
+					return;
+				}
+			}
+		}
+	}
+
+	void State::BroadcastPush(StateType stateType)
+	{
+		StateBroadcast message(StateAction::PUSH, stateType);
+		Broadcast(message);
+	}
+
+	void State::BroadcastPop()
+	{
+		StateBroadcast message(StateAction::POP, StateType::NO_STATE);
+		Broadcast(message);
+	}
+
+	void State::BroadcastClear()
+	{
+		StateBroadcast message(StateAction::CLEAR, StateType::NO_STATE);
+		Broadcast(message);
+	}
+
+	void State::Broadcast(const StateBroadcast& message)
+	{
+		for (auto it = m_Listeners.begin(); it != m_Listeners.end(); ++it)
+		{
+			if (auto stp = it->lock())
+			{
+				stp->VListen(message);
+			}
+		}
 	}
 
 	void RunState(State& state, SDL_Window& window, std::function<bool()> exitFunction)
--- Source/Starboard_2013/Starboard_2013/State.h
+++ Source/Starboard_2013/Starboard_2013/State.h
@@ -3,6 +3,8 @@
 
 #include <SDL_atomic.h>
 
+#include <memory>
+#include <vector>
 #include <functional>
 
 struct SDL_Surface;
@@ -10,22 +12,59 @@ struct SDL_Window;
 
 namespace temm
 {
-	class StateStack;
+
+	enum class StateAction
+	{
+		PUSH,
+		POP,
+		CLEAR,
+	};
+
+	enum class StateType
+	{
+		NO_STATE,
+		TEST_STATE,
+	};
+
+	struct StateBroadcast
+	{
+		StateAction action;
+		StateType type;
+		StateBroadcast(StateAction action, StateType type)
+			: action(action)
+			, type(type)
+		{}
+	};
+
+	class IStateListener
+	{
+	public:
+		~IStateListener() {}
+		virtual void VListen(const StateBroadcast& message) = 0;
+	};
 
 	class State
 	{
 	public:
 		State();
-		State(StateStack& ss);
-
 		~State() {}
 
 		virtual bool VProcessInput() = 0;
 		virtual bool VUpdate(Uint32 dt) = 0;
 		virtual void VDraw(SDL_Surface& surface) = 0;
 
+		void AttachListener(std::shared_ptr<IStateListener> pListener);
+		void DetachListener(std::shared_ptr<IStateListener> pListener);
+
+	protected:
+		void BroadcastPush(StateType stateType);
+		void BroadcastPop();
+		void BroadcastClear();
+
 	private:
-		StateStack* m_pStateStack;
+		void Broadcast(const StateBroadcast& message);
+
+		std::vector<std::weak_ptr<IStateListener>> m_Listeners;
 	};
 
 	void RunState(State& state, SDL_Window& surface, std::function<bool()> exitFunction);
