efcc6a3b300255c417bfdddc8887629e08fa8403
evinstk <tanner.evins@gmail.com>
2014-09-06 14:20:22 -0500

    Have main() use the TestState

--- Source/Starboard_2013/Starboard_2013/main.cpp
+++ Source/Starboard_2013/Starboard_2013/main.cpp
@@ -1,33 +1,27 @@
 #include "SDL_Initialization.h"
+#include "TestState.h"
 #include "Clock.h"
 #include <SDL.h>
+#include <iostream>
 
 int main(int argc, char* argv[])
 {
-	temm::SDL_Initialization initObj(SDL_INIT_VIDEO);
-	std::shared_ptr<SDL_Window> window(SDL_CreateWindow("Starboard",
-		SDL_WINDOWPOS_UNDEFINED,
-		SDL_WINDOWPOS_UNDEFINED,
-		1024,
-		768,
-		SDL_WINDOW_SHOWN), SDL_DestroyWindow);
-
-	Uint32 timeSinceLastUpdate = 0;
-	Uint32 timePerFrame = 17;
-	bool bQuit = false;
-	temm::Clock clock;
-	while (!bQuit)
+	try
 	{
-		Uint32 dt = clock.restart();
-		timeSinceLastUpdate += dt;
-		while (timeSinceLastUpdate > timePerFrame)
-		{
-			timeSinceLastUpdate -= timePerFrame;
-			// processInput();
-			// update(timePerFrame);
-		}
-		bQuit = true;
-		// render();
+		temm::SDL_Initialization initObj(SDL_INIT_VIDEO);
+		std::shared_ptr<SDL_Window> window(SDL_CreateWindow("Starboard",
+			SDL_WINDOWPOS_UNDEFINED,
+			SDL_WINDOWPOS_UNDEFINED,
+			1024,
+			768,
+			SDL_WINDOW_SHOWN), SDL_DestroyWindow);
+
+		temm::TestState state;
+		RunState(state, *window.get(), []{ return false; });
+	}
+	catch (std::exception e)
+	{
+		std::cerr << e.what() << std::endl;
 	}
 
 	return 0;
