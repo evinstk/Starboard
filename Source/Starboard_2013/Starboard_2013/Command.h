14774d6dd81f77accd8312bbc9c4a632f4f7ae2c
Tanner Evins <tanner.evins@gmail.com>
2014-09-06 16:41:49 -0500

    Store command actions in variables (no subclass)
    This strategy is borrowed from the SFML book. Subclass implementation runs
    into problems at the high-level IGameObject interface: More often than
    not, more functionality is needed than what a high-level class can offer.
    
    Instead, store functions in variables that support high-level interface.
    Use template function MakeAction to hide functionality for a derived
    GameObject class behind a base class interface.
    
    A mechanism will need to be implemented to simulate a kind of RTTI. At
    current state, Command objects are liable to unsafe casts.

--- Source/Starboard_2013/Starboard_2013/Command.h
+++ Source/Starboard_2013/Starboard_2013/Command.h
@@ -1,18 +1,35 @@
 #ifndef _COMMAND_H_
 #define _COMMAND_H_
 
+#include <functional>
+#include <cassert>
+
 namespace temm
 {
 
 	class IGameObject;
 
-	class ICommand
+	struct Command
 	{
-	public:
-		virtual ~ICommand() {}
-		virtual void VExecute(IGameObject& gameObject) = 0;
-		virtual void VUndo(IGameObject& gameObject) {}
+		typedef std::function<void(IGameObject&, Uint32 dt)> Action;
+
+		Action action;
+		Action undo;
+		Command(Action action, Action undo = [](IGameObject&, Uint32){})
+			: action(action)
+			, undo(undo)
+		{}
 	};
+
+	template<class DerivedObject, typename Function>
+	Command::Action MakeAction(Function fn)
+	{
+		return[=](GameObject& gameObject, Uint32 dt)
+		{
+			assert(dynamic_cast<DerivedObject*>(&gameObject) != nullptr);
+			fn(static_cast<DerivedObject&>(gameObject), dt);
+		}
+	}
 
 }
 
